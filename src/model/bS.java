2c8c77d7afe5fc249429e929a8c8499b16caa35e
endimion <triantafyllou.ni@gmail.com>
2014-12-23 13:43:24 +0200

    new version of bs

--- src/model/bS.java
+++ src/model/bS.java
@@ -1,71 +1,74 @@
-package model;
+package model; 
 
 public class bS {
 
-	private /*@ spec_public @*/ myArray ma = new myArray();
-	
-	//@ public invariant ma.the_array != null  && 0 <= ma.the_array.length && (\typeof(ma.the_array) == \type(int[])) ;
-	
-	public int the_value ;
-	
-	private /*@ spec_public @*/ int the_left ; 
-	private /*@ spec_public @*/ int the_right ; 
-	private /*@ spec_public @*/ int the_pivot ; 
-	private /*@ spec_public @*/ boolean the_found ; 
-	private /*@ spec_public @*/ int the_return ;
-	
-	
-	
-	public bS(){
-		the_left = 0;
-		ar().setSize(9);
-		the_right = ma.getSize() ;
-		
-	}
-	
-	public /*@ pure @*/ myArray ar(){ return ma ;}
-	
-	
-	public /*@ pure @*/ int pivot(){return the_pivot; }
-	public /*@ pure @*/ int value(){return the_value;}
-	public /*@ pure @*/ int left(){return the_left;}
-	public /*@ pure @*/ int right(){return the_right;}
-	public /*@ pure @*/ boolean found(){return the_found ;} 
-	public  /*@ pure @*/ int toRetrun(){return the_return;}
-	
+private /*@ spec_public @*/ myArray ma = new myArray();
+
+//@ public invariant ma.the_array != null  && 0 <= ma.the_array.length && (\typeof(ma.the_array) == \type(int[])) ;
+
+public int the_value ;
+
+private /*@ spec_public @*/ int the_left ; 
+private /*@ spec_public @*/ int the_right ; 
+private /*@ spec_public @*/ int the_pivot ; 
+private /*@ spec_public @*/ boolean the_found ; 
+private /*@ spec_public @*/ int the_return ;
 
 
-	/*@ requires (found() == false) && ( ar().getIndex(pivot()) < value()) && ( left() != right())
-	  @ && (pivot()  >= 0) && (right() >= 0) && (left() >= 0) ;
-	  @ ensures (left() == \old(pivot() +1)) && (right() == \old(right())) 
-	  @ && (found() == (\old(ar().getIndex( (pivot() +1 + right()) / 2)))  == \old(value())) 
-	  @ && (pivot() == \old( (pivot() +1 + right()) / 2) ) 
-	  @ && ( (\old(ar().getIndex( (pivot() +1 + right()) / 2)))  == \old(value())  ==> toRetrun() == \old((pivot() +1 + right()) / 2) ) 
-	  @ && ( (\old(ar().getIndex( (pivot() +1 + right()) / 2)))  != \old(value())  ==> toRetrun() == -1 ) ;
-	  @*/
+
+public bS(){}
+
+public /*@ pure @*/ myArray ar(){ return ma ;}
+
+
+public /*@ pure @*/ int pivot(){return the_pivot; }
+public /*@ pure @*/ int value(){return the_value;}
+public /*@ pure @*/ int left(){return the_left;}
+public /*@ pure @*/ int right(){return the_right;}
+public /*@ pure @*/ boolean found(){return the_found ;} 
+public  /*@ pure @*/ int toRetrun(){return the_return;}
+
+
+
+
+
+
+	/*@ requires 
+	  @         (found() == false) && 
+	  @         ( ar().getIndex(pivot()) < value()) && 
+	  @         ( left() != right()) &&
+	  @         (pivot()  >= 0) && 
+	  @         (right() >= 0) && 
+	  @         (left() >= 0) ;
+	  @ ensures 
+	  @          (left() == \old(pivot() +1)) && 
+	  @          (right() == \old(right()))   &&
+	  @          (found() == (\old(ar().getIndex( (pivot() +1 + right()) / 2)))  == \old(value())) && 
+	  @          (pivot() == \old( (pivot() +1 + right()) / 2) ) &&
+	  @          ( (\old(ar().getIndex( (pivot() +1 + right()) / 2)))  == \old(value())  ==> 
+	  @             toRetrun() == \old((pivot() +1 + right()) / 2) ) &&
+	  @          ( (\old(ar().getIndex( (pivot() +1 + right()) / 2)))  != \old(value())  ==> 
+	  @             toRetrun() == -1 ) ;
+	*/
 	public void  searchRight(){
-		if ( ( the_found == false) && ( ar().getIndex(the_pivot) < the_value) && ( the_left != the_right)
-				&&(ma != null)  && (the_pivot >= 0) && (the_left >= 0) && (the_right >= 0)	
-		){
-			the_left = the_pivot +1 ;
+		
+		if(ma != null && the_found == false  && ar().getIndex(the_pivot) < value()
+			&& left()!= right() && pivot() >= 0 && left()>=0 && right()>=0){
 			
-			if ( ar().getIndex((the_pivot +1 + the_right) / 2) == the_value ){
-				the_found = true ;
-			}else{the_found = false ;}
+			the_left = pivot() +1;
 			
-			if( ar().getIndex((the_pivot +1 + the_right) / 2) == value() ){
-				the_return = (the_pivot +1 + the_right) / 2 ;
-			}else{ the_return =  -1;}
+			if( ar().getIndex( (pivot() +1 + right()) / 2) == value())
+			{ 	the_found = true;
+				the_return = (pivot() +1 + right()) / 2 ;
+			}
+			else{ 
+				the_found = false;
+				the_return = -1;
+			} 
 			
-			the_pivot = (the_pivot +1 + the_right) / 2 ;
-			
-			
-			
-		}
-	
-		if(the_left == the_right){the_found = true ;}
-	
-	}//end of searchright
+			the_pivot = (the_pivot + 1 + the_right) /2 ;
+		}//end of requires if
+	}//end of searchRight
 
 
 
@@ -74,37 +77,37 @@ public class bS {
 
 
 
-	
-	/*@ requires (found() == false) && ( ar().getIndex(pivot()) > value()) && ( left() != right())
-	@ && (pivot()  >= 0) && (right() >= 0) && (left() >= 0) ;
-	@ ensures (right() == \old(pivot() -1)) && (left() == \old(left())) 
-	@ && (found() == (\old(ar().getIndex( (pivot() -1 + left()) / 2)))  == \old(value())) 
-	@ && (pivot() == \old( (pivot() -1 + left()) / 2) )
-	@ && ( (\old(ar().getIndex( (pivot() -1 + left()) / 2)))  == \old(value())  ==> toRetrun() == \old((pivot() -1 + left()) / 2) ) ; 
-	@ && ( (\old(ar().getIndex( (pivot() -1 + left()) / 2)))  != \old(value())  ==> toRetrun() == -1 ) ;
-	@*/
-	public void  searchLeft(){
-		if ( ( the_found == false) && ( ar().getIndex(the_pivot) > the_value) && ( the_left != the_right)
-				&&(ma != null)  && (the_pivot >= 0) && (the_left >= 0) && (the_right >= 0)	
-		){
-			the_right = the_pivot -1 ;
-			
-			if ( ar().getIndex((the_pivot -1 + the_left) / 2) == the_value ){
-				the_found = true ;
-			}else{the_found = false ;}
-			
-			if( ar().getIndex((the_pivot -1 + the_left) / 2) == value() ){
-				the_return = (the_pivot -1 + the_left) / 2 ;
-			}else{ the_return =  -1;}
-			
-			the_pivot = (the_pivot -1 + the_left) / 2 ;
-		}
-	
-		if(the_left == the_right){the_found = true ;}
-	
-	}//end of searchleft
-	
+
+/*@ requires (found() == false) && ( ar().getIndex(pivot()) > value()) && ( left() != right())
+@ && (pivot()  >= 0) && (right() >= 0) && (left() >= 0) ;
+@ ensures  (right() == \old(pivot() -1)) && (left() == \old(left())) 
+@ && (found() == (\old(ar().getIndex( (pivot() -1 + left()) / 2)))  == \old(value())) 
+@ && (pivot() == \old( (pivot() -1 + left()) / 2) )
+@ && ( (\old(ar().getIndex( (pivot() -1 + left()) / 2)))  == \old(value())  ==> toRetrun() == \old((pivot() -1 + left()) / 2) ) 
+@ && ( (\old(ar().getIndex( (pivot() -1 + left()) / 2)))  != \old(value())  ==> toRetrun() == -1 ) ; 
+@*/
+public void  searchLeft(){
+	if ( ( the_found == false) && ( ar().getIndex(the_pivot) > the_value) && ( the_left != the_right)
+			&&(ma != null)  && (the_pivot >= 0) && (the_left >= 0) && (the_right >= 0)	
+	){
+		the_right = the_pivot -1 ;
+		
+		if ( ar().getIndex((the_pivot -1 + the_left) / 2) == the_value ){
+			the_found = true ;
+		}else{the_found = false ;}
+		
+		if( ar().getIndex((the_pivot -1 + the_left) / 2) == value() ){
+			the_return = (the_pivot -1 + the_left) / 2 ;
+		}else{ the_return =  -1;}
+		
+		the_pivot = (the_pivot -1 + the_left) / 2 ;
+	}
+}
 
 
 
-}//end of class
+
+}
+
+
+
--- src/model/myArray.java
+++ src/model/myArray.java
@@ -1,4 +1,7 @@
 package model;
+
+
+
 public class myArray{
 
 private /*@ spec_public @*/ int[] the_array = new int[0];
@@ -28,41 +31,35 @@ public /*@ pure @*/ int getSize(){
 
 
 
-/*@ requires (I > 0)  && (getSize() == 0) ;
-  @ ensures (getSize() == I)  &&  
-  @ (\forall int i;  getIndex(i) == \old(getIndex(i)) );
-  @*/
-public void setSize(int I){
-	if( (I > 0) && (the_array.length == 0)   )
-	the_array = new int[I];
-	
-}
-
-
-
-/*@ requires (I1 > 0) && (getSize() > 0) && (I2 < getSize()) && (I2 > 0)
-  @ && (getIndex(A,I2 -1) < I1) 
-  @ && ( ((I2 + 1) < getSize(A)) && (getIndex(A,I2 + 1) != -1) ==> getIndex(A,I2 +1) > I1)  
-  @  ;
-  @ ensures (\forall int i; (i == I2) ==> getIndex(i) == I1) 
-  @ && (\forall int i; (i != I2) ==> getIndex(i) == \old(getIndex(i))) ;
-  @ also 
-  @ requires !((I1 > 0) && (getSize() > 0) && (I2 < getSize()) && (I2 > 0)
-  @ && (getIndex(A,I2 -1) < I1) && ( ((I2 + 1) < getSize(A)) ==> getIndex(A,I2 +1) > I1  ))  ;
-  @ assignable \nothing ;
-  @  
- */
-public void setIndex(int I1, int I2){
-	if ((I2 < this.getSize() ) && (this.getSize() > 0 ) && (I1 > 0) && (I2 > 0)) 
-	{	if( getIndex(I2 -1) < I1) 
-			if( (I2 +1 < getSize()) && (getIndex(I2 +1) != -1)){
-				if( getIndex(I2+1) > I1) the_array[I2] = I1 ;
-			}else the_array[I2] = I1;
-	}
+	/*@ requires (I > 0)  && (getSize() == 0) ;
+	  @ ensures (getSize() == I)  &&  
+	  @ (\forall int i;  getIndex(i) == \old(getIndex(i)) );
+	  @
+	  */
+	public void setSize(int I){
+		if( (I > 0) && (the_array.length == 0)   )
+		the_array = new int[I];
 		
-	 
-	
-}
+	} 
+
+
+
+	/*@ 
+	  @ requires ( ( val > 0) && (getSize() > 0) && (pos < getSize()) && (pos > 0)
+	  @ && (getIndex(pos -1) < val)  && ( ((pos + 1) < getSize()) && (getIndex(pos + 1) != -1)) 
+	  @ && getIndex(pos +1) > val)  ;
+	  @ ensures (\forall int i; (i == pos) ==> getIndex(i) == val) 
+	  @ && (\forall int i; (i != pos) ==> getIndex(i) == \old(getIndex(i))) ;
+	  */
+	public void setIndex(int val, int pos){
+		if (val > 0 && getSize() > 0 && pos < getSize() && pos >0 
+				&& getIndex(pos -1) < val && pos +1 <getSize() && getIndex(pos + 1) != -1
+			 && getIndex(pos +1) > val	){
+			
+			the_array[pos] = val;
+			
+		}//end of if	
+	}//end of setIndex
 
 
 }
